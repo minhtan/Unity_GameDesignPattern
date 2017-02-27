0aeaa40060b2e8a8d66878403278d844bd069ebf
Tan <tan@Tans-iMac.local>
2017-02-27 17:38:15 +0700

    command pattern

---
+++ Assets/Command/Command.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Command {
+	public abstract void Execute ();
+}
+
+public class MoveCommand : Command{
+	public override void Execute ()
+	{
+		Debug.Log ("move");
+	}
+}
+
+public class FireCommand : Command{
+	public override void Execute ()
+	{
+		Debug.Log ("fire");
+	}
+}
---
+++ Assets/Command/InputHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputHandler : MonoBehaviour {
+
+	Command key1;
+	Command key2;
+
+	// Use this for initialization
+	void Start () {
+		key1 = new MoveCommand ();
+		key2 = new FireCommand ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.M)) {
+			key1.Execute ();
+		}
+		if (Input.GetKeyDown(KeyCode.F)) {
+			key2.Execute ();
+		}
+	}
+}
